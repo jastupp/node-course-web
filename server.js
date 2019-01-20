5b869fbb5df1811153c88e95a8b87638f9dc1d1b
John Stupples <john.stupples@virginaustralia.com>
2019-01-20 18:36:02 +1000

    Added files

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use((request, response, next) => {
+    const now = new Date().toString();
+    console.log(`${now}: ${request.method} ${request.url}`);
+    next();
+});
+
+app.use((request, response, next) => {
+    response.render('maintenance.hbs');
+});
+
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('year', () => new Date().getFullYear());
+
+app.get('/', (request, response) => {
+    response.render('home.hbs', {
+        title: 'Home Page Title',
+        welcome: 'Welcome to the home page'
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        title: 'Page Title'
+    });
+});
+
+app.get('/bad', (request, response) => {
+    response.send({error: 'Bad Message'});
+});
+
+app.listen(3000, () => console.log('Server listening pon port 3000'));
+
+
