d0096051ac2fc4769f2a1681f0a194b5aae4cd80
John Stupples <john.stupples@virginaustralia.com>
2019-01-22 07:47:36 +1000

    Removed Maint page

--- server.js
+++ server.js
@@ -16,9 +16,6 @@ app.use((request, response, next) => {
     next();
 });
 
-app.use((request, response, next) => {
-    response.render('maintenance.hbs');
-});
 
 
 app.use(express.static(__dirname + '/public'));
